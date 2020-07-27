cce63d8a46adc7cd22821c6cfe2d484a03a9d76a
MarcusNerva <hadrianus_1@163.com>
2020-07-27 21:50:23 +0800

    init commit

--- scripts/test_scripts/test_cuda.py
+++ scripts/test_scripts/test_cuda.py
@@ -1,7 +1,10 @@
 import torch
+import torchvision
 
 if __name__ == '__main__':
-    if torch.cuda.is_available():
-        print('hhhhh')
-    else:
-        print('oh no!')
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    model.eval()
+    x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+    predictions = model(x)
+
+    print(predictions)
