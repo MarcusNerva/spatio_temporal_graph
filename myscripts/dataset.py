b04bd062b3d7f310a4f719b45b0332d959454532
MarcusNerva <hadrianus_1@163.com>
2020-08-11 09:42:24 +0800

    dataset module is created, but process I3D features first

---
+++ myscripts/dataset.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding=utf-8
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import json
+import h5py
+import os
+import sys
+sys.path.append('../')
+from tools import get_total_settings
+
+if __name__ == "__main__":
+    opts = get_total_settings()
+    data_path = opts.data_path
+    scene_path = os.path.join(data_path, 'scene')
+    scene_I3D_dir = os.path.join(scene_path, 'I3D')
+    scene_I3D_path = os.path.join(scene_path, 'I3D/I3D_feats.hdf5')
+
+    object_path = os.path.join(data_path, 'object')
+
+    I3D_feats = h5py.File(scene_I3D_path, 'r')
+    vid_store = []
+
+    for key in I3D_feats.keys():
+        vid = int(key[3:])
+        vid = 'video' + str(int(key[3:]) - 1)
+
+        temp = I3D_feats[key][()]
+        length = temp.shape[0]
+        idxs = np.round(np.linspace(0, length, 10, endpoint=False)).astype(np.int32)
+        temp = temp[idxs]
+        temp_path = os.path.join(scene_I3D_dir, vid + '.npy')
+        np.save(temp_path, temp)
+
