dc933989bfd3736700a8b1f290ab7b33a2e5d9d7
MarcusNerva <hadrianus_1@163.com>
2020-08-13 11:20:57 +0800

    solve some problems

--- myscripts/extract_scene_features.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python extract_scene_features.py --data_dir /Users/bismarck/Downloads/temp_data --output_dir /Users/bismarck/Downloads/temp_data/scene --video_path /Users/bismarck/Downloads/temp_data/videos
