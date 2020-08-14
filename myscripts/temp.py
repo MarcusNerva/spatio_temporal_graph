64998788a21b78741d8395dc7060acabc2143040
MarcusNerva <hadrianus_1@163.com>
2020-08-14 10:02:41 +0800

    do experiment

--- myscripts/temp.py
+++ myscripts/temp.py
@@ -24,6 +24,9 @@ class Experiment(nn.Module):
 if __name__ == '__main__':
 
     exper = Experiment()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    exper.to(device)
+
     print(exper.get_device_origin())
     print(exper.get_device_generate())
 
