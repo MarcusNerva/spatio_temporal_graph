635e6aefc26a0392787c4b2984fdfc0fff6a77bf
MarcusNerva <hadrianus_1@163.com>
2020-08-14 09:59:37 +0800

    do experiment

--- myscripts/temp.py
+++ myscripts/temp.py
@@ -1,33 +1,29 @@
 import numpy as np
 import torch
+import torch.nn as nn
 import glob
 import os
 import json
 import pickle
-np.set_printoptions(threshold=np.inf)
-# from collections import defaultdict
 
-def _construct_G_st(spatial_matrix, temporal_matrix):
+class Experiment(nn.Module):
+    def __init__(self):
+        super(Experiment, self).__init__()
+        self.register_buffer('blank_seq', torch.ones((3, 3)))
 
-    spatial_matrix = torch.from_numpy(spatial_matrix)
-    temporal_matrix = torch.from_numpy(temporal_matrix)
+    def generate(self):
+        self.register_buffer('full_seq', torch.zeros((3, 3)))
 
-    N = 50
-    G_st = torch.zeros((N, N))
-    for i, j in zip(range(10), range(0, N, 5)):
-        G_st[j : j + 5, j : j + 5] = spatial_matrix[i]
-    for i, j in zip(range(9), range(5, N, 5)):
-        G_st[j - 5 : j, j : j + 5] = temporal_matrix[i]
+    def get_device_origin(self):
+        return self.blank_seq.device
 
-    return G_st.cpu().numpy().astype(np.int32)
+    def get_device_generate(self):
+        self.generate()
+        return self.full_seq.device
 
 if __name__ == '__main__':
-    spatial_matrix = np.ones((10, 5, 5))
-    temporal_matrix = np.ones((9, 5, 5)) * 2
 
-    G_st = _construct_G_st(spatial_matrix, temporal_matrix)
-    for item in G_st:
-        temp = str()
-        for idx in item:
-            temp += str(idx);
-        print(temp)
+    exper = Experiment()
+    print(exper.get_device_origin())
+    print(exper.get_device_generate())
+
